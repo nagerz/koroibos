97704d3c7e38dd167004febdc08c5e87955ec68b
nagerz <znager@gmail.com>
2019-06-05 01:51:53 -0600

    Adds retrieving youngest and oldest olympian specs. Refactors youngest and oldest endpoint responses to match spec and dry code to use only one line for successful responses.

--- specs/endpoints/olympians.spec.js
+++ specs/endpoints/olympians.spec.js
@@ -9,7 +9,7 @@ describe('Olympians API', () => {
       specHelper.testSetup()
     });
 
-    test('it should return a successful request', () => {
+    test('it should return a successful request for all olympians', () => {
       return request(app).get('/api/v1/olympians').then(response => {
         expect(response.status).toBe(200)
         expect(response.body.olympians.length).toBe(6)
@@ -18,6 +18,30 @@ describe('Olympians API', () => {
         expect(response.body.olympians[0].age).toBe(21)
         expect(response.body.olympians[0].sport).toBe("taekwondo")
         expect(response.body.olympians[0].total_medals_won).toBe(1)
+      })
+    })
+
+    test('it should return a successful request for youngest olympian', () => {
+      return request(app).get('/api/v1/olympians?age=youngest').then(response => {
+        expect(response.status).toBe(200)
+        expect(response.body.youngest_olympian.length).toBe(1)
+        expect(response.body.youngest_olympian[0].name).toBe("Patimat Abakarova")
+        expect(response.body.youngest_olympian[0].team).toBe("Azerbaijan")
+        expect(response.body.youngest_olympian[0].age).toBe(21)
+        expect(response.body.youngest_olympian[0].sport).toBe("taekwondo")
+        expect(response.body.youngest_olympian[0].total_medals_won).toBe(1)
+      })
+    })
+
+    test('it should return a successful request for oldest olympian', () => {
+      return request(app).get('/api/v1/olympians?age=oldest').then(response => {
+        expect(response.status).toBe(200)
+        expect(response.body.oldest_olympian.length).toBe(1)
+        expect(response.body.oldest_olympian[0].name).toBe("Luc Abalo")
+        expect(response.body.oldest_olympian[0].team).toBe("France")
+        expect(response.body.oldest_olympian[0].age).toBe(31)
+        expect(response.body.oldest_olympian[0].sport).toBe("handball")
+        expect(response.body.oldest_olympian[0].total_medals_won).toBe(1)
       })
     })
   })
